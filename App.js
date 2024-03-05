06bbdd4ac5c904bf10a0b4038d1a27a7e828f2a9
ZeevHaydar <nzhifhaidar@gmail.com>
2024-03-05 09:26:00 +0700

    small edit

--- App.js
+++ App.js
@@ -1,10 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated, SafeAreaView } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font'
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(()=> {
+    async function prepare() {
+      try {
+          
+      }
+      catch (e) {
+        console.warn(e);
+      }
+      finally {
+        setAppIsReady(true);
+        fadeOut();
+      }
+    }
+
+    prepare();
+  }, []);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 3 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
+    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+      <Animated.View style={{ opacity: fadeAnim }}>
+    </Animated.View>
     <HomeScreen/>
+    </SafeAreaView>
+    
+    
   );
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,37 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useCallback, useEffect, useState } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font'
+import { StyleSheet, Text, View, Animated } from 'react-native';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
 
 export default function HomeScreen() {
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  useEffect(()=> {
-    async function prepare() {
-      try {
-        await new Promise(resolve => setTimeout(resolve, 4000));  
-      }
-      catch (e) {
-        console.warn(e);
-      }
-      finally {
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
+  
   return (
     <View style={styles.container}>
       <Text>Welcome to Jasakula!</Text>
