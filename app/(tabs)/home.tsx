d4dc19a8bd8e53e4b231c74dda8bba8a7d9de0f2
ZeevHaydar <nzhifhaidar@gmail.com>
2024-05-07 14:30:44 +0700

    changing all to type script

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import HomeScreen from '../../../screens/HomeScreen'
+import HomeScreen from '../../screens/HomeScreen'
 
 const Page = () => {
   return (
